6184a0f4ffccb0a7986cc272fb58b39fc2959eaf
bregarulai <bregarulai@gmail.com>
2022-06-11 19:12:51 -0400

    created postsData file

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { postsData } from '../../data/postData';
+
 const Post = () => {
-  return <div className='post'>Post</div>;
+  return (
+    <div className='post'>
+      {postsData.map((post, i) => (
+        <div>{post.desc}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Post;
