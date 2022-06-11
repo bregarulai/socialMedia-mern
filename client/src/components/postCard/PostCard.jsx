7c46c3a0cd88d10e8baed35956ce680a36fc7496
bregarulai <bregarulai@gmail.com>
2022-06-11 19:42:28 -0400

    created style for postCard component

--- client/src/components/postCard/PostCard.jsx
+++ client/src/components/postCard/PostCard.jsx
@@ -11,9 +11,13 @@ const PostCard = ({ post }) => {
     <div className='postCard'>
       <img src={img} alt={name} />
       <div className='postCard__reactions'>
-        <img src={liked ? heartImg : notLikeImg} alt='not liked icon' />
-        <img src={commentImg} alt='comment icon' />
-        <img src={shareImg} alt='share icon' />
+        <img
+          className='postCard__icon'
+          src={liked ? heartImg : notLikeImg}
+          alt='not liked icon'
+        />
+        <img className='postCard__icon' src={commentImg} alt='comment icon' />
+        <img className='postCard__icon' src={shareImg} alt='share icon' />
       </div>
       <span>{`${likes} likes`}</span>
       <div className='postCard__details'>
