094ae112c4900730cc37f2339a2b89623e7dbdcd
bregarulai <bregarulai@gmail.com>
2022-06-11 19:16:23 -0400

    created PostCard component

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import { postsData } from '../../data/postData';
+import { PostCard } from '..';
 
 const Post = () => {
   return (
     <div className='post'>
       {postsData.map((post, i) => (
-        <div>{post.desc}</div>
+        <PostCard key={post.name + i} post={post} />
       ))}
     </div>
   );
---
+++ client/src/components/postCard/PostCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostCard = ({ post }) => {
+  return <div className='postCard'>{post.desc}</div>;
+};
+
+export default PostCard;
