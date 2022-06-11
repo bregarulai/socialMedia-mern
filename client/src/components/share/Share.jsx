c8371904b04068e3b9b183a2f3359b7d9eb61a86
bregarulai <bregarulai@gmail.com>
2022-06-11 18:20:27 -0400

    created jsx and styled for share component

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  UilScenery,
+  UilPlayCircle,
+  UilLocationPoint,
+  UilSchedule,
+} from '@iconscout/react-unicons';
 
 import profileImg from '../../img/profileImg.jpg';
 
@@ -6,6 +12,27 @@ const Share = () => {
   return (
     <div className='share'>
       <img src={profileImg} alt='profile' />
+      <div>
+        <input type='text' placeholder="What's happening" />
+        <div className='share__options'>
+          <div className='share__option share__option--photo'>
+            <UilScenery />
+            <span>Photo</span>
+          </div>
+          <div className='share__option share__option--video'>
+            <UilPlayCircle />
+            <span>Video</span>
+          </div>
+          <div className='share__option share__option--location'>
+            <UilLocationPoint />
+            <span>Location</span>
+          </div>
+          <div className='share__option share__option--schedule'>
+            <UilSchedule />
+            <span>Schedule</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
