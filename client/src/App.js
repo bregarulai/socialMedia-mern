e85ffb54ef23f3f1ad02949baf4dfed9d8262044
bregarulai <bregarulai@gmail.com>
2022-06-11 11:38:01 -0400

    created home page and its structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import { Home } from './pages';
+
 function App() {
   return (
     <div className='app'>
       <div className='app__blur' style={{ top: '-3%', right: 0 }}></div>
       <div className='app__blur' style={{ top: '36%', left: '-5rem' }}></div>
+      <Home />
     </div>
   );
 }
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className='home__profile'>Profile</div>
+      <div className='home__posts'>Posts</div>
+      <div className='home__trending'>Trending</div>
+    </div>
+  );
+};
+
+export default Home;
