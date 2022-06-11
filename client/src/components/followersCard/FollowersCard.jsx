cfb13d57cf545f9f5f9aaf35094dc0bb1fb222cf
bregarulai <bregarulai@gmail.com>
2022-06-11 13:37:13 -0400

    created followersData file

--- client/src/components/followersCard/FollowersCard.jsx
+++ client/src/components/followersCard/FollowersCard.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const FollowersCard = () => {
-  return <div className='followersCard'>FollowersCard</div>;
+  return (
+    <div className='followersCard'>
+      <h3>Who is following you</h3>
+    </div>
+  );
 };
 
 export default FollowersCard;
