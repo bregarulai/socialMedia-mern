5ca8159988b3bf75387622e06828e197ce54571e
bregarulai <bregarulai@gmail.com>
2022-06-11 12:20:23 -0400

    adde jsx and styled to logoSearch component

--- client/src/components/logoSearch/LogoSearch.jsx
+++ client/src/components/logoSearch/LogoSearch.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { UilSearch } from '@iconscout/react-unicons';
 
 import logo from '../../img/logo.png';
 
@@ -6,6 +7,12 @@ const LogoSearch = () => {
   return (
     <div className='logoSearch'>
       <img src={logo} alt='logo' />
+      <div className='logoSearch__search'>
+        <input type='text' placeholder='#Explore' />
+        <div className='logoSearch__s-icon'>
+          <UilSearch />
+        </div>
+      </div>
     </div>
   );
 };
