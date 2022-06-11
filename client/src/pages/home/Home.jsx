12d407ac8874e42a048b0b5eb5d5e93ae3b5bf83
bregarulai <bregarulai@gmail.com>
2022-06-11 11:42:11 -0400

    created profile component

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Profile } from '../../components';
 
 const Home = () => {
   return (
     <div className='home'>
-      <div className='home__profile'>Profile</div>
+      <div className='home__profile'>
+        <Profile />
+      </div>
       <div className='home__posts'>Posts</div>
       <div className='home__trending'>Trending</div>
     </div>
