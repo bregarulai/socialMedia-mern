e02b779cdd1100b2b16a7d272b4bea0da036a7b9
bregarulai <bregarulai@gmail.com>
2022-06-11 20:33:46 -0400

    created jsx for trendCard component

--- client/src/components/trendingCard/TrendingCard.jsx
+++ client/src/components/trendingCard/TrendingCard.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 import { trendData } from '../../data/trendData';
 
 const TrendingCard = () => {
-  return <div className='trendingCard'>TrendingCard</div>;
+  return (
+    <div className='trendingCard'>
+      <h3>Trends for you</h3>
+      {trendData.map((trend, i) => (
+        <div key={trend.name + i} className='trendingCard__trend'>
+          <span>#{trend.name}</span>
+          <span>{`${trend.shares}k shares`}</span>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TrendingCard;
