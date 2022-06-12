8174ddcd0dc6de78f71992c9da0277921fa25953
bregarulai <bregarulai@gmail.com>
2022-06-11 20:46:32 -0400

    created profile page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import { Home } from './pages';
+import { Home, Profile } from './pages';
 
 function App() {
   return (
     <div className='app'>
       <div className='app__blur' style={{ top: '-3%', right: 0 }}></div>
       <div className='app__blur' style={{ top: '36%', left: '-5rem' }}></div>
-      <Home />
+      {/* <Home /> */}
+      <Profile />
     </div>
   );
 }
