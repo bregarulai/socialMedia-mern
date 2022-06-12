4af5ac49d20fbb840ae348a6cb0cde64930e217a
bregarulai <bregarulai@gmail.com>
2022-06-11 21:31:38 -0400

    created left side for profile page

--- client/src/components/infoCard/InfoCard.jsx
+++ client/src/components/infoCard/InfoCard.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { UilPen } from '@iconscout/react-unicons';
 
 const InfoCard = () => {
-  return <div className='infoCard'>InfoCard</div>;
+  return (
+    <div className='infoCard'>
+      <div className='infoCard__header'>
+        <h4>Your Information</h4>
+        <div>
+          <UilPen width='2.4rem' height='1.6rem' />
+        </div>
+      </div>
+      <div className='infoCard__info'>
+        <span className='infoCard__infoTitle'>
+          <b>Status</b>
+        </span>
+        <span>In Relationship</span>
+      </div>
+      <div className='infoCard__info'>
+        <span className='infoCard__infoTitle'>
+          <b>Lives in</b>
+        </span>
+        <span>Seattle</span>
+      </div>
+      <div className='infoCard__info'>
+        <span className='infoCard__infoTitle'>
+          <b>Works at</b>
+        </span>
+        <span>Google</span>
+      </div>
+      <button className='app__button infoCard__button'>Logout</button>
+    </div>
+  );
 };
 
 export default InfoCard;
