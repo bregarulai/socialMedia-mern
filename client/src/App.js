4ff9a816041a7b83d32914cb85f03102c7b5a19b
bregarulai <bregarulai@gmail.com>
2022-06-11 21:52:32 -0400

    created auth page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Home, Profile } from './pages';
+import { Home, Profile, Auth } from './pages';
 
 function App() {
   return (
@@ -6,7 +6,8 @@ function App() {
       <div className='app__blur' style={{ top: '-3%', right: 0 }}></div>
       <div className='app__blur' style={{ top: '36%', left: '-5rem' }}></div>
       {/* <Home /> */}
-      <Profile />
+      {/* <Profile /> */}
+      <Auth />
     </div>
   );
 }
