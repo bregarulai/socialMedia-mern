23b2511dfad0f7e1402e87afd43325c05bfabdaa
bregarulai <bregarulai@gmail.com>
2022-06-11 20:22:18 -0400

    created nav icons in trending component

--- client/src/components/trending/Trending.jsx
+++ client/src/components/trending/Trending.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
+import homeIcon from '../../img/home.png';
+import notiIcon from '../../img/noti.png';
+import commentIcon from '../../img/comment.png';
+import { UilSetting } from '@iconscout/react-unicons';
+
 const Trending = () => {
-  return <div className='trending'>Trending component</div>;
+  return (
+    <div className='trending'>
+      <div className='trending__navIcons'>
+        <img className='trending__navIcon' src={homeIcon} alt='home nav icon' />
+        <UilSetting />
+        <img
+          className='trending__navIcon'
+          src={notiIcon}
+          alt='notification nav icon'
+        />
+        <img
+          className='trending__navIcon'
+          src={commentIcon}
+          alt='comment nav icon'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Trending;
