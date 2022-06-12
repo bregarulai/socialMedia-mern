104eb1d227a7d2659514c9c0d9270ce8fc6d2abd
bregarulai <bregarulai@gmail.com>
2022-06-11 22:13:08 -0400

    created left section in auth page

--- client/src/pages/auth/Auth.jsx
+++ client/src/pages/auth/Auth.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import logo from '../../img/logo.png';
+
 const Auth = () => {
-  return <div className='auth'>Auth</div>;
+  return (
+    <div className='auth'>
+      <div className='auth__leftSection'>
+        <img className='auth__logo' src={logo} alt='logo' />
+        <div className='auth__appName'>
+          <h1>BCM Social</h1>
+          <h6>Explore the ideas throughout the world</h6>
+        </div>
+      </div>
+      <div className='auth__rightSection'>
+        <h1>Form</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
